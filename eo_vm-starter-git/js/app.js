76523d4540242392f5ea55c94e501aeef91476e9
Anthony Le Cigne <dev@lecigne.net>
2018-05-14 22:37:03 +0200

    [eo] Ajout des pièces de monnaie

--- eo_vm-starter-git/js/app.js
+++ eo_vm-starter-git/js/app.js
@@ -14,6 +14,70 @@ var machine = new VendingMachine();
 /**
  * Liste des types de pièces de monnaie acceptées par le distributeur.
  */
+var Quarter = /** @class */ (function () {
+    function Quarter() {
+        this._value = 0.25;
+    }
+    Object.defineProperty(Quarter.prototype, "value", {
+        get: function () {
+            return this._value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Quarter.prototype.getImageUrl = function () {
+        return "./img/Quarter.png";
+    };
+    return Quarter;
+}());
+var Dime = /** @class */ (function () {
+    function Dime() {
+        this._value = 0.10;
+    }
+    Object.defineProperty(Dime.prototype, "value", {
+        get: function () {
+            return this._value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Dime.prototype.getImageUrl = function () {
+        return "./img/Dime.png";
+    };
+    return Dime;
+}());
+var Half = /** @class */ (function () {
+    function Half() {
+        this._value = 0.50;
+    }
+    Object.defineProperty(Half.prototype, "value", {
+        get: function () {
+            return this._value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Half.prototype.getImageUrl = function () {
+        return "./img/Half.png";
+    };
+    return Half;
+}());
+var Dollar = /** @class */ (function () {
+    function Dollar() {
+        this._value = 1.00;
+    }
+    Object.defineProperty(Dollar.prototype, "value", {
+        get: function () {
+            return this._value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Dollar.prototype.getImageUrl = function () {
+        return "./img/Dollar.jpg";
+    };
+    return Dollar;
+}());
 /***
  * Fonction factory qui renvoie des instances de produits aléatoirement.
  */
--- eo_vm-starter-git/ts/coins.ts
+++ eo_vm-starter-git/ts/coins.ts
@@ -1,3 +1,51 @@
 /**
  * Liste des types de pièces de monnaie acceptées par le distributeur.
  */
+
+ class Quarter {
+     private _value = 0.25;
+
+     get value(): number {
+         return this._value;
+     }
+
+     getImageUrl(): string {
+         return "./img/Quarter.png";
+     }
+ }
+
+ class Dime {
+     private _value = 0.10;
+
+     get value(): number {
+         return this._value;
+     }
+
+     getImageUrl(): string {
+         return "./img/Dime.png";
+     }
+ }
+
+ class Half {
+     private _value = 0.50;
+
+     get value(): number {
+         return this._value;
+     }
+
+     getImageUrl(): string {
+         return "./img/Half.png";
+     }
+ }
+
+ class Dollar {
+     private _value = 1.00;
+
+     get value(): number {
+         return this._value;
+     }
+
+     getImageUrl(): string {
+         return "./img/Dollar.jpg";
+     }
+ }
