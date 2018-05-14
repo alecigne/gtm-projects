fe3d0b004d268d73f9e78f3c9e8fb011a0cee3d2
Anthony Le Cigne <dev@lecigne.net>
2018-05-14 22:16:16 +0200

    [eo] Ecriture de tsconfig.json et compilation

---
+++ eo_vm-starter-git/js/app.js
@@ -0,0 +1,23 @@
+/**
+ * Fichier principal de l'application.
+ */
+var VendingMachine = /** @class */ (function () {
+    function VendingMachine() {
+    }
+    return VendingMachine;
+}());
+/// <reference path="vending-machine.ts"/>
+var machine = new VendingMachine();
+/**
+ * Liste des catégories de produits.
+ */
+/**
+ * Liste des types de pièces de monnaie acceptées par le distributeur.
+ */
+/***
+ * Fonction factory qui renvoie des instances de produits aléatoirement.
+ */
+/**
+ * Liste des produits vendus dans le distributeur.
+ */
+//# sourceMappingURL=app.js.map
