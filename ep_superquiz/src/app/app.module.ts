31cce6df668e0009da04095ce38990e6698f785d
Anthony Le Cigne <dev@lecigne.net>
2018-05-15 14:22:54 +0200

    [ep] Ajout des premiers composants

--- ep_superquiz/src/app/app.module.ts
+++ ep_superquiz/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { HomeComponent } from './common/home/home.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent, HomeComponent, FooterComponent ],
   imports: [ BrowserModule ],
   bootstrap: [AppComponent]
 })
