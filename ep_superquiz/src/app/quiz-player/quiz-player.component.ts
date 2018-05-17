1d3e8b6ce13a9aefa4835b834d83eceb1b00301a
Anthony Le Cigne <dev@lecigne.net>
2018-05-17 09:54:12 +0200

    [ep] Ajout du QuizStateManager

--- ep_superquiz/src/app/quiz-player/quiz-player.component.ts
+++ ep_superquiz/src/app/quiz-player/quiz-player.component.ts
@@ -1,41 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { QUIZZES } from '../data/quizzes';
 import { Answer } from '../models/answer';
-import { AnswersState } from '../quiz-state-manager.service';
+import { AnswersState, QuizStateManager } from '../quiz-state-manager.service';
 import { QuizService } from '../quiz.service';
 import { Quiz } from '../models/quiz';
 import { Question } from '../models/question';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-quiz-player',
   templateUrl: './quiz-player.component.html',
-  styles: []
+  styles: [],
+  // On utilise le QSM localement
+  providers: [ QuizStateManager ]
 })
 export class QuizPlayerComponent implements OnInit {
-  currentQuiz: Quiz;
-  currentQuestion: Question;
-  currentAnswer: Answer;
-  currentAnswers: AnswersState;
+  // Les propriétés deviennent des observables
+  currentQuiz: Observable<Quiz>;
+  currentQuestion: Observable<Question>;
+  currentAnswer: Observable<Answer>;
+  currentAnswers: Observable<AnswersState>;
   isStarted: boolean = false;
 
-  constructor(private quizService: QuizService) {}
+  constructor(
+    private quizService: QuizService,
+    private quizStateManager: QuizStateManager) {}
 
   ngOnInit() {
+    // Ces objets sont désormais fournis par le service
+    this.currentQuiz = this.quizStateManager.getCurrentQuiz();
+    this.currentQuestion = this.quizStateManager.getCurrentQuestion();
+    this.currentAnswer = this.quizStateManager.getCurrentAnswer();
+    this.currentAnswers = this.quizStateManager.getCurrentAnswers();
     // Charge le quiz sur lequel l'utilisateur à cliquer
     // L'ID du quiz à charger viendra de l'URL
     const quizId = 32;
-    this.currentQuiz = this.quizService.loadQuiz(quizId);
-    this.currentQuestion = this.currentQuiz.questions[0];
-    this.currentAnswer = new Answer({questionId: this.currentQuestion.id});
-    this.currentAnswers = {};
+    const quiz = this.quizService.loadQuiz(quizId);
+    // Pousse le quiz chargé dans l'observable 'currentQuiz'
+    this.quizStateManager.setQuiz(quiz);
+  }
+
+  onAnswered(answer: Answer) {
+    // console.log('Réponse reçue', answer);
+    // @TODO: Sauvegarde la réponse dans le service QuizStateManager
+    this.quizStateManager.addAnswer(answer);
+  }
+
+  gotoNextQuestion() {
+    // @TODO: utilise le QuizStateManager pour aller à la question suivante
+    this.quizStateManager.gotoNextQuestion();
+  }
+
+  gotoPreviousQuestion() {
+    // @TODO: utilise le QuizStateManager pour aller à la question précédente
+    this.quizStateManager.gotoPreviousQuestion();
   }
 
   startQuiz() {
     this.isStarted = true;
-  }
-
-  onAnswered(answer: Answer) {
-    console.log('Réponse reçue', answer);
+    // @TODO: utilise le QuizStateManager pour aller à la première question
+    this.quizStateManager.gotoFirstQuestion();
   }
 
 }
