10ec0f342dea76dcfa82b1a834ebf6b049fb6761
Anthony Le Cigne <dev@lecigne.net>
2018-05-18 15:22:37 +0200

    [er] Mise au point du composant client

--- er_proxibanque/src/app/client/client-list/client-list.component.ts
+++ er_proxibanque/src/app/client/client-list/client-list.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client-list',
-  templateUrl: './client-list.component.html',
-  styleUrls: ['./client-list.component.css']
+  templateUrl: './client-list.component.html'
 })
 export class ClientListComponent implements OnInit {
 
